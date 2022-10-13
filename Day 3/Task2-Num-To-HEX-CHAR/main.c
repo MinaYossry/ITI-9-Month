1cddaf4ab98ab446d21e1fb4470725a39ce5d3a6
Mina Yossry <mina.yossry4@gmail.com>
2022-10-13 14:51:31 +0200

    Add files via upload

---
+++ Day 3/Task2-Num-To-HEX-CHAR/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Task 2 - Convert Number to HEX and Character
+    int num;
+    scanf("%d", &num);
+    printf("HEX: 0x%x\n", num);
+    printf("char: %c\n", num);
+    return 0;
+}
---
+++ Day 3/Task3-Char-To-ASCI/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Task 3 : Convert Char to ASCI
+    char c;
+    scanf("%c", &c);
+    printf("ASCI: %d\n", c);
+    return 0;
+}
